1f66ba5685c215c900aa3ef6f73fc53d9efc8007
reedsearle <reed_searle@hotmail.com>
2024-03-10 15:05:28 -0600

    Started on controls and settings xml

---
+++ app/src/main/java/edu/cnm/deepdive/leavemealone/controller/ControlsFragment.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive.leavemealone.controller;
+
+import android.view.View;
+import androidx.fragment.app.Fragment;
+
+public class ControlsFragment extends Fragment {
+
+
+  public void soundAlarm(View v){
+// TODO: 3/10/2024 Find alarm type and send to speaker 
+    // TODO: 3/10/2024 get current gps location and send to unsecure  
+    // TODO: 3/10/2024 take picture from both cameras 
+    // TODO: 3/10/2024 get email and send location and picture 
+  }
+
+  public void armAlarm(View v){
+// TODO: 3/10/2024 set countdown timer so phone can be set down
+    // TODO: 3/10/2024 activate motin sensing 
+  }
+
+  public void disarmAlarm(View v) {
+// TODO: 3/10/2024 de activate motion sensing 
+  }
+
+  public void setSecure(View v) {
+// TODO: 3/10/2024 get current location and send to secure database 
+  }
+
+  public void setUnsecure(View v) {
+// TODO: 3/10/2024 get current location and send to unsecure database 
+  }
+}
