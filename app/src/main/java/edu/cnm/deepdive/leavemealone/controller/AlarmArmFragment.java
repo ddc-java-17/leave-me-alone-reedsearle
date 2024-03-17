c116847414f081cf4234c6511e24a1d2aedef32a
reedsearle <reed_searle@hotmail.com>
2024-03-17 17:12:32 -0600

    temporary buttons installed and running

--- app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmArmFragment.java
+++ app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmArmFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import edu.cnm.deepdive.leavemealone.R;
 import edu.cnm.deepdive.leavemealone.databinding.FragmentAlarmArmBinding;
@@ -33,10 +34,10 @@ public class AlarmArmFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    NavController navController = Navigation.findNavController(view);
+    binding.motion.setOnClickListener(
+        (b)-> navController.navigate(AlarmArmFragmentDirections.navigateWarning()));
     binding.disarmAlarm.setOnClickListener(
-        (button)-> Navigation
-            .findNavController(button)
-            .navigate(ControlsFragmentDirections
-                .navigateControls()) );
+        (v)-> navController.navigate(ControlsFragmentDirections.navigateControls()) );
   }
 }
--- app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmWarningFragment.java
+++ app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmWarningFragment.java
@@ -7,17 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import edu.cnm.deepdive.leavemealone.R;
 import edu.cnm.deepdive.leavemealone.databinding.FragmentAlarmArmBinding;
+import edu.cnm.deepdive.leavemealone.databinding.FragmentAlarmWarningBinding;
 
 /**
- * A simple {@link Fragment} subclass. Use the {@link AlarmWarningFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link Fragment} subclass. Use the {@link AlarmWarningFragment#newInstance} factory
+ * method to create an instance of this fragment.
  */
 public class AlarmWarningFragment extends Fragment {
 
-  private FragmentAlarmArmBinding binding;
+  private FragmentAlarmWarningBinding binding;
 
   public AlarmWarningFragment() {
     // Required empty public constructor
@@ -26,17 +28,17 @@ public class AlarmWarningFragment extends Fragment {
   @Override
   public View onCreateView(
       LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    binding = FragmentAlarmArmBinding.inflate(inflater, container, false);
+    binding = FragmentAlarmWarningBinding.inflate(inflater, container, false);
     return binding.getRoot();
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    NavController navController = Navigation.findNavController(view);
+    binding.codeTimeOut.setOnClickListener(
+        (v) -> navController.navigate(AlarmWarningFragmentDirections.navigateSounding()));
     binding.disarmAlarm.setOnClickListener(
-        (button)-> Navigation
-            .findNavController(button)
-            .navigate(ControlsFragmentDirections
-                .navigateControls()) );
+        (v) -> navController.navigate(ControlsFragmentDirections.navigateControls()));
   }
 }
