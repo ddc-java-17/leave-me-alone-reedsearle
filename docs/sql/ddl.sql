d40325b1aa9c8508c1b79576d94a2bc2eeabf210
reedsearle <reed_searle@hotmail.com>
2024-03-18 15:34:45 -0600

    formatted DDL

--- docs/sql/ddl.sql
+++ docs/sql/ddl.sql
@@ -1,11 +1,24 @@
 -- Generated 2024-03-18 15:10:58-0600 for database version 1
 
-CREATE TABLE IF NOT EXISTS `alert` (`alert_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `triggered` INTEGER NOT NULL, `timestamp` INTEGER NOT NULL, `photoname` TEXT);
+CREATE TABLE IF NOT EXISTS `alert`
+(
+    `alert_id`  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `triggered` INTEGER                           NOT NULL,
+    `timestamp` INTEGER                           NOT NULL,
+    `photoname` TEXT
+);
 
 CREATE INDEX IF NOT EXISTS `index_alert_triggered` ON `alert` (`triggered`);
 
 CREATE INDEX IF NOT EXISTS `index_alert_timestamp` ON `alert` (`timestamp`);
 
-CREATE TABLE IF NOT EXISTS `location` (`location_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `alert_id` INTEGER NOT NULL, `secure` INTEGER NOT NULL, `latitude` REAL, `longitude` REAL);
+CREATE TABLE IF NOT EXISTS `location`
+(
+    `location_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    `alert_id`    INTEGER                           NOT NULL,
+    `secure`      INTEGER                           NOT NULL,
+    `latitude`    REAL,
+    `longitude`   REAL
+);
 
 CREATE INDEX IF NOT EXISTS `index_location_secure` ON `location` (`secure`);
