1fddd6757116ed75af9f4169bb5878792b258326
reedsearle <reed_searle@hotmail.com>
2024-04-02 11:34:22 -0600

    Trying no back button

--- app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmArmFragment.java
+++ app/src/main/java/edu/cnm/deepdive/leavemealone/controller/AlarmArmFragment.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.leavemealone.controller;
 
 import android.os.Bundle;
 import android.text.TextWatcher;
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -34,6 +35,17 @@ public class AlarmArmFragment extends Fragment {
 
   public AlarmArmFragment() {
     // Required empty public constructor
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    OnBackPressedCallback callback = new OnBackPressedCallback(false) {
+      @Override
+      public void handleOnBackPressed() {
+
+      }
+    };
   }
 
   @Override
